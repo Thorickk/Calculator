19e9c49dd8b9a87588768f12374e89f01db16f8e
Thorickk <michnikiewicz@o2.pl>
2024-05-28 17:38:14 +0200

    add styles, add useState and main functions, add event handlers to buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,59 @@
+import { useState } from 'react'
 import styles from './App.module.css'
 import { Button } from './components/Button/Button'
 
 
 function App() {
+const [result, setResult] = useState('')
+
+
+function handleButtonClick(e) {
+	console.log(e.target.value)
+	setResult((prevResult) =>  prevResult + e.target.value)
+}
+
+function handleClear() {
+	setResult('')
+}
+
+function handleDeleteLastDigit() {
+	setResult((prevResult) => prevResult.slice(0,-1))
+}
+
+function calculateNumbers() {
+	setResult((prevResult) => eval(prevResult))
+}
+
+
+
 	return (
 		<div className={styles.calculator}>
 			<button className={styles.history}>📝</button>
 			<div className={styles.display}>
-				<div className={styles.operation}>3+2=</div>
-				<div className={styles.result}>5</div>
+				{/* <div className={styles.operation}>{result}</div> */}
+				<div className={styles.result}>{result}</div>
 			</div>
 			<div className={styles.buttons}>
-				<Button style={{backgroundColor: "orangered"}}>C</Button>
-				<Button style={{backgroundColor: "tomato"}}>del</Button>
+				<Button onClick={handleClear} style={{backgroundColor: "orangered"}}>C</Button>
+				<Button onClick={handleDeleteLastDigit} style={{backgroundColor: "tomato"}}>⌫</Button>
 				<Button>%</Button>
-				<Button>/</Button>
-				<Button>7</Button>
-				<Button>8</Button>
-				<Button>9</Button>
-				<Button>x</Button>
-				<Button>4</Button>
-				<Button>5</Button>
-				<Button>6</Button>
-				<Button>-</Button>
-				<Button>1</Button>
-				<Button>2</Button>
-				<Button>3</Button>
-				<Button>+</Button>
+				<Button onClick={handleButtonClick}>/</Button>
+				<Button onClick={handleButtonClick}>7</Button>
+				<Button onClick={handleButtonClick}>8</Button>
+				<Button onClick={handleButtonClick}>9</Button>
+				<Button onClick={handleButtonClick}>*</Button>
+				<Button onClick={handleButtonClick}>4</Button>
+				<Button onClick={handleButtonClick}>5</Button>
+				<Button onClick={handleButtonClick}>6</Button>
+				<Button onClick={handleButtonClick}>-</Button>
+				<Button onClick={handleButtonClick}>1</Button>
+				<Button onClick={handleButtonClick}>2</Button>
+				<Button onClick={handleButtonClick}>3</Button>
+				<Button onClick={handleButtonClick}>+</Button>
 				<Button>+/-</Button>
-				<Button>0</Button>
-				<Button>.</Button>
-				<Button style={{backgroundColor: "#5da2fd"}}>=</Button>
+				<Button onClick={handleButtonClick}>0</Button>
+				<Button onClick={handleButtonClick}>.</Button>
+				<Button onClick={calculateNumbers} style={{backgroundColor: "#5da2fd"}}>=</Button>
 			</div>
 		</div>
 	)
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,9 @@
 import styles from './Button.module.css'
 
-export function Button({ children, style }) {
+export function Button({ children, style, onClick}) {
 	return (
 		<>
-			<button className={styles.btn} style={style}>{children}</button>
+			<button onClick={onClick} className={styles.btn} style={style} value={children}>{children}</button>
 		</>
 	)
 }
